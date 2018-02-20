5df5ab46eb8c8b2f06a0aaea94fefc38a9cbdd38
jimmy johnson <jimmy@jnjohnson.io>
2018-02-20 16:20:16 -0500

    introduce new file to add a scrolling effect to pressing the button in the hero section

---
+++ Portfolio-website/public/js/scroll-effect.js
@@ -0,0 +1,7 @@
+$(".hero-button").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});

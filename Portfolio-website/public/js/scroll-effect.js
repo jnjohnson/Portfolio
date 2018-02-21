fe8353ba3859cc6e13560628aa13eec5dcbcb76d
jimmy johnson <jimmy@jnjohnson.io>
2018-02-21 01:15:37 -0500

    changed speed of scroll effect, and gave scroll effect to all nav links

--- Portfolio-website/public/js/scroll-effect.js
+++ Portfolio-website/public/js/scroll-effect.js
@@ -3,5 +3,13 @@ $(".hero-button").click(function(e) {
     var section = $(this).attr("href");
     $("html, body").animate({
         scrollTop: $(section).offset().top
-    });
+    }, 'slow');
+});
+
+$(".nav-link").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    }, 'slow');
 });

54e17493b730e569223e803a10f2da96c2317f1f
jimmy johnson <jimmy@jnjohnson.io>
2018-02-22 23:18:36 -0500

    introduce environment variable to node startup

--- Portfolio-website/index.js
+++ Portfolio-website/index.js
@@ -1,13 +1,20 @@
 /* SET UP YOUR MAIN VARIABLES */
-var express = require('express'),
-	config 	= require('./server/configure'),
-	app 	= express()
+var express 	= require('express'),
+	config 		= require('./server/configure'),
+	app 		= express(),
+	environment = process.argv[2];
 
 /* CALL THE MODULE.EXPORTS CONSTRUCTOR FUNCTION OF THE CONFIGURE FILE THIS ADDS TO APP AND RETURNS APP
 THIS IS DONE SO WE DO NOT HAVE TO WRITE A BUNCH OF CODE IN OUR INDEX FILE. */
 app = config(app);
 
 /* SET THE PORT */
+if (environment === undefined){
+	process.env.PORT = 80;
+}
+else if (environment == "dev"){
+	process.env.PORT = 3000;
+}
 app.set('port',process.env.PORT || 3000);
 
 /* MAKE THE VIEWS DIRECTORY SO WE CAN SERVE UP THE FILES WITHIN THAT DIRECTORY */

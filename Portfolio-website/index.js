e4709812ba6f6f581f64980f33641e53ddfaec63
jimmy johnson <jimmy@jnjohnson.io>
2018-02-24 00:33:20 -0500

    production server is now https

--- Portfolio-website/index.js
+++ Portfolio-website/index.js
@@ -8,19 +8,35 @@ var express 	= require('express'),
 THIS IS DONE SO WE DO NOT HAVE TO WRITE A BUNCH OF CODE IN OUR INDEX FILE. */
 app = config(app);
 
-/* SET THE PORT */
-if (environment === undefined){
-	process.env.PORT = 80;
-}
-else if (environment == "dev"){
-	process.env.PORT = 3000;
-}
-app.set('port',process.env.PORT || 3000);
-
 /* MAKE THE VIEWS DIRECTORY SO WE CAN SERVE UP THE FILES WITHIN THAT DIRECTORY */
 app.set('views', __dirname + '/views');
 
-/* LISTEN ON PORT 3000 */
-app.listen(app.get('port'),function(){
-	console.log('Portfolio server up...');
-});
+if (environment === undefined){
+	const hostname 	= 'jnjohnson.io',
+		  port		= 443;
+
+	var	https		= require('https'),
+		fs 			= require('fs'),
+		options		= {
+			ca: 	fs.readFileSync('/etc/ssl/private/COMODO_DV_SHA-256_bundle.crt'),
+			key:	fs.readFileSync('/etc/ssl/private/jnjohnson.io.key'),
+			cert:	fs.readFileSync('/etc/ssl/private/jnjohnson_io.crt')
+		};
+	
+	app.set('port', port);
+
+	https.createServer(options, app).listen(app.get('port'), function(){
+		console.log('Server up: jnjohnson.io is live on port ' + app.get('port'));
+	});
+}
+else if (environment == "dev"){
+	app.set('port', 3000);
+
+	/* LISTEN ON PORT 3000 */
+	app.listen(app.get('port'),function(){
+		console.log('Portfolio dev server is up on port ' + app.get('port'));
+	});
+}
+else {
+	console.log('error, ' + environment + ' is not a valid option');
+}
